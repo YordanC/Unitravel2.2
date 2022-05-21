fe54af81b0e907621644a9ed55f5503ead3ee37d
Yordan Caicedo <yordane.caicedoc@uqvirtual.edu.co>
2022-05-21 01:12:31 -0500

    "proyecto web, index"

--- web/src/main/java/co/edu/uniquindio/unitravel/bean/InicioBean.java
+++ web/src/main/java/co/edu/uniquindio/unitravel/bean/InicioBean.java
@@ -1,2 +1,18 @@
-package co.edu.uniquindio.unitravel.bean;public class InicioBean {
+package co.edu.uniquindio.unitravel.bean;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+import javax.faces.view.ViewScoped;
+import java.io.Serializable;
+
+@Component
+@ViewScoped
+@Getter
+@Setter
+public class InicioBean implements Serializable {
+private String mensaje= "mi primera pagina en JFS";
+
+
 }
